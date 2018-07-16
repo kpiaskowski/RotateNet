c9bd0fb42db21842682f0fd87368727e2e860315
kpiaskowski <kar.piaskowski@gmail.com>
2018-07-16 10:23:07 +0200

    DatasetAPI

--- dataproviders/chairs_provider.py
+++ dataproviders/chairs_provider.py
@@ -1,103 +1,64 @@
+import tensorflow as tf
 import os
-import random
-import time
-
-import cv2
 import numpy as np
+import random
 
 
 class ChairProvider:
-    def __init__(self, path, n_examples, n_imgs=5, split_ratio=0.8, seed=789432):
-        """
-        :param path: path to rendered chairs directory
-        :param img_size: desired size of images, they will be resized to match this size
-        :param n_imgs: number of images in single training example
-        :param batch_size: size of batch
-        :param n_examples: number of predefined examples in the whole dataset
-        :param split_ratio: split train/val set according to this ratio
-        """
-        self.n_examples = n_examples
-        self.path = path
+    def __init__(self, path, batch_size, img_size, n_imgs=5, split_ratio=0.8, num_parallel_calls=12, seed=789432):
+        self.batch_size = batch_size
+        self.n_imgs = n_imgs
+        self.img_size = img_size
+        self.num_parallel_calls = num_parallel_calls
+        self.class_paths = sorted([os.path.join(path, name) for name in os.listdir(path) if '.mat' not in name])
 
-        self.names_angles = self.read_filenames(self.path)
-
-        print('Generating data...')
-        s = time.time()
-        self.train, self.val = self.generate_examples(n_examples, self.names_angles, n_imgs, split_ratio, seed)
-        e = time.time()
-        print("Data generated in {:.2f} seconds".format(e - s))
-
-    def get_batch(self, batch_size, type, img_size):
-        """
-        Reads single batch from disc.
-        :param batch_size: number of examples in batch (note that batch = batchsize * n_imgs_per_batch)
-        :param type: 'val' or 'train'
-        :param img_size: size of output image
-        :return:
-        """
-        imnames, angles = self.train if type == 'train' else self.val
-        datalen = len(imnames)
-
-        ids = [random.randint(0, datalen - 1) for _ in range(batch_size)]
-        batch_angles = [angles[i] for i in ids]
-        batches_names = [imnames[i] for i in ids]
-        imgs = [[cv2.resize(np.array(cv2.imread(name), np.float32), (img_size, img_size)) for name in batch_name] for batch_name in batches_names]
-        return imgs, batch_angles
-
-    def read_filenames(self, dir):
-        """
-        Reads all names from the root directory of chairs dataset. Groups them by their class.
-        :param dir: root chairs directory
-        """
-        class_names = sorted((name for name in os.listdir(dir) if 'all_chair_names.mat' not in name))
-
-        data = []
-        for i, class_name in enumerate(class_names):
-            class_path = os.path.join(dir, class_name, 'renders')
-            filepaths = sorted((os.path.join(class_path, filename) for filename in os.listdir(class_path)))
-            angles = [self.read_angle_from_name(path) for path in filepaths]
-            data.append((filepaths, angles))
-
-        return data
-
-    def read_angle_from_name(self, name):
-        """
-        Reads the vector of angles from filename
-        :param name: string with filename, could be either full path or relative path
-        Returns: list of 3 angles
-        """
-        trans_dict = str.maketrans({'t': '', 'p': '', 'r': ''})
-        angles = np.array(name.split('/')[-1].rstrip('.png').translate(trans_dict).split('_')[2:], np.float)
-        return angles
-
-    def generate_examples(self, n_examples, data, n_imgs, split_ratio, seed):
-        """
-        Generates examples randomly by grouping their names into single list. Pickles generated data to speed up the future queries.
-        :param seed: used in random generators
-        :param n_imgs: number of images and angles per examples
-        :param filenames: dictionary with data
-        :return:
-        """
+        ids = list(range(len(self.class_paths)))
         rng = random.Random(seed)
-        img_paths = []
-        relative_angles = []
+        rng.shuffle(ids)
 
-        datalen = len(data)
-        for i in range(n_examples):
-            cls = rng.randint(0, datalen - 1)
-            class_data = data[cls]
-            names, angles = class_data[0], class_data[1]
+        split_point = int(len(self.class_paths) * split_ratio)
+        self.train_paths = self.class_paths[:split_point]
+        self.val_paths = self.class_paths[split_point:]
 
-            class_len = len(names)
-            ids = [rng.randint(0, class_len - 1) for _ in range(n_imgs)]
+    def load_images(self, filename):
+        image_string = tf.read_file(filename)
+        image_decoded = tf.image.decode_jpeg(image_string)
+        image_resized = tf.image.resize_images(image_decoded, [self.img_size, self.img_size])
+        return image_resized, filename
 
-            paths = [names[i] for i in ids]
-            base_angle = angles[ids[0]]
-            angles = [angles[i] - base_angle for i in ids]
+    def decode_string(self, img, filename):
+        decoded = filename.decode()
+        trans_dict = str.maketrans({'t': '', 'p': '', 'r': ''})
+        split_str = decoded.split('/')
 
-            img_paths.append(paths)
-            relative_angles.append(angles)
+        angles = np.array(split_str[-1].rstrip('.png').translate(trans_dict).split('_')[2:], np.float32)
+        class_path = '/'.join(split_str[:4])
+        class_id = np.int32(self.class_paths.index(class_path))
 
-        # train / val
-        split_point = int(n_examples * split_ratio)
-        return (img_paths[:split_point], relative_angles[:split_point]), (img_paths[split_point:], relative_angles[split_point:])
+        return img, angles, class_id
+
+    def create_dataset(self, paths):
+        dataset = tf.data.Dataset.from_tensor_slices((tf.constant(paths)))
+        dataset = dataset.shuffle(buffer_size=5000)
+        dataset = dataset.flat_map(lambda dir: tf.data.Dataset.list_files(dir + '/renders/*.png').
+                                   map(self.load_images, self.num_parallel_calls).
+                                   map(lambda img, filename: tuple(tf.py_func(self.decode_string, [img, filename], [tf.float32, tf.float32, tf.int32], stateful=False)), self.num_parallel_calls).
+                                   batch(self.n_imgs))
+        dataset = dataset.filter(lambda x, y, z: tf.equal(tf.shape(y)[0], self.n_imgs))
+        dataset = dataset.prefetch(self.batch_size)
+        dataset = dataset.batch(self.batch_size)
+        dataset = dataset.repeat()
+        return dataset
+
+    def dataset(self):
+        t_d = self.create_dataset(self.train_paths)
+        v_d = self.create_dataset(self.val_paths)
+
+        h = tf.placeholder(tf.string, shape=[])
+        iter = tf.data.Iterator.from_string_handle(h, t_d.output_types, t_d.output_shapes)
+        images, angles, classes = iter.get_next()
+
+        t_iter = t_d.make_one_shot_iterator()
+        v_iter = v_d.make_one_shot_iterator()
+
+        return h, t_iter, v_iter, images, angles, classes
--- train_chairs.py
+++ train_chairs.py
@@ -1,42 +1,52 @@
-import datetime
-
-import tensorflow as tf
-
 from architectures.AE_only_conv import AE_only_conv
 from dataproviders.chairs_provider import ChairProvider
+import tensorflow as tf
+import datetime
+import time
+
 from utils.functions import normalize_inputs
 
-chairs_path = '../RotateNet_data/chairs'
 batch_size = 50
 learning_rate = 0.0005
-n_examples = 1000
-n_imgs = 2
+n_imgs = 5
 img_size = 128
-epochs = 30
+iters = 50000
 ckpt = 20
+save_ckpt = 2000
 activation = tf.nn.relu
 note = 'mse_loss'
 
+# dataprovider
+dataprovider = ChairProvider('../RotateNet_data/chairs', batch_size=batch_size, img_size=img_size, n_imgs=n_imgs)
+handle, t_iter, v_iter, images, angles, classes = dataprovider.dataset()
+
+# naming
 model = AE_only_conv()
-model_name = 'chairs|--|{}|--|batch_{}|--|eta_{}|--|nexamples_{}|--|imgsize_{}|--|epochs_{}|--|activ_{}|--|nimgs_{}|--|date_{}|--|'.format(model.name, batch_size, learning_rate, n_examples,
-                                                                                                                                       img_size, epochs, activation.__name__, n_imgs,
-                                                                                                                                       datetime.datetime.now(), note)
+model_name = 'chairs|--|{}|--|batch_{}|--|eta_{}|--|imgsize_{}|--|activ_{}|--|nimgs_{}|--|date_{}|--|{}'.format(model.name,
+                                                                                                                batch_size,
+                                                                                                                learning_rate,
+                                                                                                                img_size,
+                                                                                                                activation.__name__,
+                                                                                                                n_imgs,
+                                                                                                                datetime.datetime.now(),
+                                                                                                                note)
 
-provider = ChairProvider(chairs_path, n_examples=n_examples, n_imgs=n_imgs)
-
-imgs = tf.placeholder(tf.float32, [None, None, img_size, img_size, 3])
-angles = tf.placeholder(tf.float32, [None, None, 3])
+# # placeholders
 is_training = tf.placeholder(tf.bool)
 
-normalized_imgs, normalized_angles = normalize_inputs(imgs, angles)
-
+# data
+normalized_imgs, normalized_angles = normalize_inputs(images, angles)
 base_imgs, target_imgs = model.split_imgs(normalized_imgs)
 target_angles = normalized_angles[:, -1, :]
 
-lv = model.encoder(base_imgs, activation, is_training)
-merged_lv = model.merge_lv_angle(lv, target_angles, activation)
+# model
+reshaped_imgs = tf.reshape(base_imgs, [-1, img_size, img_size, 3])  # tf hack
+reshaped_angles = tf.reshape(target_angles, [-1, 3])  # tf hack
+lv = model.encoder(reshaped_imgs, activation, is_training)
+merged_lv = model.merge_lv_angle(lv, reshaped_angles, activation)
 gen_imgs = model.decoder(merged_lv, activation, is_training)
 
+# losses
 mse_loss = tf.losses.mean_squared_error(labels=target_imgs, predictions=gen_imgs)
 
 # summaries
@@ -46,6 +56,7 @@ img_summary = tf.summary.image('images', concat_img)
 loss_merged = tf.summary.merge([loss_summary])
 img_merged = tf.summary.merge([img_summary])
 
+# train ops
 update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 with tf.control_dependencies(update_ops):
     optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
@@ -57,27 +68,26 @@ saver = tf.train.Saver(max_to_keep=1)
 with tf.Session() as sess:
     train_writer = tf.summary.FileWriter('summaries/' + model_name + '_train')
     val_writer = tf.summary.FileWriter('summaries/' + model_name + '_val')
+    t_handle, v_handle = sess.run([t_iter.string_handle(), v_iter.string_handle()])
     sess.run(tf.global_variables_initializer())
 
-    num_batches = n_examples // batch_size
-    for e in range(epochs):
-        for i in range(num_batches):
-            batch_imgs, batch_angles = provider.get_batch(batch_size, 'train', img_size)
-            _, cost, summ = sess.run([train_op, mse_loss, loss_merged], feed_dict={imgs: batch_imgs, angles: batch_angles, is_training: True})
-            print('Epoch {} of {}, train iter {} of {}, cost: {:.6f}'.format(e, epochs, i, num_batches, cost))
-            train_writer.add_summary(summ, e * num_batches + i)
+    t_s, v_s = 0, 0
+    for i in range(iters):
+        _, cost, summ = sess.run([train_op, mse_loss, loss_merged], feed_dict={handle: t_handle, is_training: True})
+        print('TRAIN iteration {} of {}, cost: {:.6f}'.format(i, iters, cost))
+        train_writer.add_summary(summ, t_s)
+        train_writer.flush()
+        t_s += 1
+
+        if t_s % ckpt == 0:
+            img_summ = sess.run(img_merged, feed_dict={handle: t_handle, is_training: False})
+            train_writer.add_summary(img_summ, t_s)
             train_writer.flush()
+            t_s += 1
 
-            if i % ckpt == 0:
-                img_summ = sess.run(img_merged, feed_dict={imgs: batch_imgs, angles: batch_angles, is_training: False})
-                train_writer.add_summary(img_summ, e * num_batches + i)
-                train_writer.flush()
-
-                batch_imgs, batch_angles = provider.get_batch(batch_size, 'val', img_size)
-                img_summ, loss_summ, cost = sess.run([img_merged, loss_merged, mse_loss], feed_dict={imgs: batch_imgs, angles: batch_angles, is_training: False})
-                print('Epoch {} of {}, val iter {} of {}, cost: {:.6f}'.format(e, epochs, i, num_batches, cost))
-                val_writer.add_summary(img_summ, e * num_batches + i)
-                val_writer.add_summary(loss_summ, e * num_batches + i)
-                val_writer.flush()
-
-                saver.save(sess, 'saved_models/' + model_name)
+            img_summ, loss_summ, cost = sess.run([img_merged, loss_merged, mse_loss], feed_dict={handle: v_handle, is_training: False})
+            print('VAL iteration {} of {}, cost: {:.6f}'.format(i, iters, cost))
+            val_writer.add_summary(img_summ, v_s)
+            val_writer.add_summary(loss_summ, v_s)
+            val_writer.flush()
+            v_s += 1
